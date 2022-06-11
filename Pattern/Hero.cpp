a412648386b07cf3dc4135d12bae3a4a5cac3ba0
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-06-11 13:45:27 +1000

    Update Hero.cpp

--- Pattern/Hero.cpp
+++ Pattern/Hero.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 #include "Person.h"
-#include "People.h"
 using namespace std;
 
-template<class T> class Stack_cont { //контейнер
+template<class T> class Stack_cont 
+{ 
 private:
     struct Key {
         T value;
@@ -13,7 +13,7 @@ private:
     
     Key *begin = NULL;
     
-    void output(Key *a) //вывод на экран содержимого стека
+    void output(Key *a)
     {
         if (a==NULL) return;
         output(a->next);
@@ -22,7 +22,7 @@ private:
   
  public:
  	
- 	void Push(T item) //добавление эл в стек
+ 	void Push(T item)
      { 
         Key *c = new Key();
         c->value=item;
@@ -30,7 +30,7 @@ private:
         begin=c;
      } 
       
-     T Pop() //инфорация о первом эл
+     T Pop()
      {
         Key *a; 
         if (begin != NULL) 
@@ -45,12 +45,12 @@ private:
         }
      }
  
-    int Empty() //проверка на пустату
+    int Empty()
     {
         return (begin==NULL);
     }
      
-    void Show() //вывод стека
+    void Show()
     {
         output(begin);
     }
@@ -65,7 +65,7 @@ template <class T> class Shaman : public Person<T>
 		T Partial_Kill(T k)
 		{
 			cout<<"The shaman joins the fight"<<endl;
-		    return (k/2); //óìåíüøàåò êîëëè÷åñòâî âðàãîâ â 2 ðàçà
+		    return k/2; //óìåíüøàåò êîëëè÷åñòâî âðàãîâ â 2 ðàçà
 		}
 };
 
@@ -89,7 +89,7 @@ template <class T> class Warrior : public Person<T>
 		T Provocation(T villain)
 		{
 			cout<<"Warrior uses provocation"<<endl;
-		    return villain/2 ; //óåíüøàåò êîëè÷åñòâî õï ïðîòèâíèêà â 2 ðàçà
+		    return villain/3 ;
 		}
 };
 
@@ -102,8 +102,13 @@ template <class T> class Ants : public Person<T>
 		
 	Ants(string v_name, T v_life, int v_damage) : Person<T>(v_name, v_life, v_damage) 
 	{
-		this->krit = krit * 2;
+		this->krit = v_damage * 2;
 	}	
+	
+	int Info_krit()
+	{
+		return krit;
+	}
 			
 	void Create_ants(int vould)
 	{
@@ -162,6 +167,33 @@ template<class T> class GameHero :  public Person<T>//, public Knight<T>, public
 		return warior.Provocation(value);
 	}
 	
+	bool Krit_damage_Ants(Ants<T> a)
+	{
+		int count = a.info_count();
+		T life = a.Info_life();
+		T buff =0;
+		
+		if (count > 0)
+		{
+			buff = life * count;
+			buff = buff - krit_damag;
+			count = buff / life;
+			a.Create_ants(count);
+			return true;
+		}
+		else return false;
+	}
+	
+	T Krit_damage_King_ants(King_ants<T> a)
+	{
+		T life = a.Info_life();
+		if (life >0)
+		{
+			T buff = life - krit_damag;
+			return buff;
+		}
+		else return 0;
+	}
 };
 
 template <class T> class Boss_ants : public Person<T>
@@ -194,21 +226,89 @@ template <class T> class Boss_ants : public Person<T>
 
 void Play(string name)
 {
+	int count = 50;
+	int buff;
 	
 	Stack_cont<string> team;
 	Shaman<int> shaman("Iron", 100, 20);
 	Knight<int> knight("God", 50, 40);
 	Warrior<int> warrior("Kavin", 150, 30);
 	GameHero<int> hero(name, 200, 30);
-	Ants<int> ants("Mur", 30, 20);
-	ants.Create_ants(50);
+	Ants<int> ants("Mur", 30, 20);		
 	King_ants<int> king("Queen", 200, 50);
 	Boss_ants<int> boss("Boss", 500, 100);
 	
+	team.Push("Baek Un-ho");
+	team.Push("Chah He-in");
 	
+	if (count > 0)
+	{
+		ants.Create_ants(count);
+		cout<<"Ok. Forward!"<<endl;
+		
+	while (count > 0)
+	{
+		count = hero.Ability_Shaman(count, shaman);
+		ants.Create_ants(count);					
+
+		count = hero.Ability_Knight(count, knight);
+		ants.Create_ants(count);
+	}
+}
+cout<<endl;
+	cout<<"Wow! It was cool!"<<endl;
+	int life = king.Info_life();
+	int life_hero = hero.Info_life();
+	
+	cout<<endl;
+	while (life > 0 && life_hero > 0)
+	{
+		cout<<"The hero goes on the attack";
+		int buff_10 = hero.Krit_damage_King_ants(king);
+		king.niw_life(buff_10);
+		life = king.Info_life();
+		cout<<" "<<life<<"Queen"<<endl;
+		
+		cout<<"Queen attacks";
+		int damage = king.Info_damage();
+		life_hero -= damage;
+		hero.niw_life(life_hero);	
+		cout<<" "<<life_hero<<"Hero"<<endl;	
+	}
+	if (life_hero >0 )
+	{
+		cout<<endl;
+		hero.Hill(hero);
+		hero.Hill(hero);
+		cout<<endl;
+		while (hero.Info_life() > 0 && boss.Info_life() > 0)
+		{
+			cout<<"The hero attacks the boss"<<endl;
+			int buff_2 = boss.Info_life();
+			int buff_3 = hero.Info_damage();
+			int buff_4 = buff_2 - buff_3;
+			boss.niw_life(buff_4);
+			
+			if (hero.Info_life() > 49)
+			{
+				boss.Krit_hero(hero,warrior,knight,shaman);
+			}
+			else
+			{
+				cout<<"The boss attacks the hero"<<endl;
+				buff_2 = hero.Info_life();
+				buff_3 = boss.Info_damage();
+				buff_4 = buff_2 - buff_3;
+				hero.niw_life(buff_4);
+			}
+		}
+		cout<<endl;
+		cout<<"The hero won"<<endl;
+	}
+	else cout<<"The hero lost"<<endl;
 }
 
-void Start()
+int main()
 {
 	string name;
 	
@@ -222,12 +322,8 @@ void Start()
 	cout<<"You have 3 assistants"<<endl;
 	cout<<"If you need their help just call them"<<endl;
 	cout<<"Be careful the monsters are very strong"<<endl;
+	cout<<endl;
 	
 	Play(name);
-}
-
-int main()
-{	
-	Start;	
 	return 0;
 }
