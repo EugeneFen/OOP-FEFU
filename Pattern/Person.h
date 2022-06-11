10d83909316409d93270b6701883f380d2bfe9b4
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-06-11 13:46:09 +1000

    Update Person.h

--- Pattern/Person.h
+++ Pattern/Person.h
@@ -6,43 +6,98 @@ template<class T> class Person {
 	
     private:
     
-	T life; //жизни
-	string name; //имя
-	int damage; //урон
+	T life; //æèçíè
+	string name; //èìÿ
+	int damage; //óðîí  !!!!!!!!!!!!!!!!!!!!!!
 	
 	public:
     
-    Myclass(string v_name, T v_life, int v_damage) //конструктор
+    Person(string v_name, T v_life, int v_damage)
     {
     	this->name = v_name;
     	this->life = v_life;
     	this->damage = v_damage;	
 	}
 
-    T Info_life() //вывод информации о жизни
+    T Info_life()
     {
     	return life;
 	}
 	
-	T niw_life(T value) //перезапись жизни
+	T niw_life(T value)
 	{
 		this->life = value;
 	}
 	
-    int Info_damage() //информация о уроне
+    int Info_damage()
     {
     	return damage;
 	}
 	
-    string Info_name() //информация о имени
+    string Info_name()
     {
     	return name;
     }
     
-    void Del() //удаление информации
+    void Del()
     {
     	this->name = 0;
     	this->life = 0;
     	this->damage = 0;
     }	
+};
+
+template<class T>
+class Stack
+{
+ 
+    struct Key {
+        T value;
+        Key *next;
+    };
+    
+    Key *begin = NULL;
+    
+    void output(Key *a)
+    {
+        if (a==NULL) return;
+        output(a->next);
+        cout << a->value << " ";
+    }
+  
+ public:
+ 	
+ 	void Push(T item)
+     { 
+        Key *c = new Key();
+        c->value=item;
+        c->next=begin;
+        begin=c;
+     } 
+      
+     T Pop()
+     {
+        Key *a; 
+        if (begin != NULL) 
+        {
+            a=begin;
+            begin=a->next;
+            return a->value;
+        }
+        else
+        {
+            throw "Stack is empty!";
+        }
+     }
+ 
+    int Empty()
+    {
+        return (begin==NULL);
+    }
+     
+    void Show()
+    {
+        output(begin);
+    }
+ 	
 };
